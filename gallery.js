cacc74df5c19147abfde5b8c62e6afc21d768de2
Aaron Conklin <conklin.aaron@gmail.com>
2017-06-12 12:48:09 -0400

    product modal with title/desc/price

--- gallery.js
+++ gallery.js
@@ -1,7 +1,12 @@
 $(function() {
 
- $('.item').click(function(event) {
+  var primitve01Description = "";
+
+ $('.gallery-item').click(function(event) {
     $('.modal-image').css('background-image', 'url("images/items/' + this.id + '.jpg")');
+    $('.modal-title').text(this.lastElementChild.lastElementChild.firstElementChild.innerHTML);
+    $('.modal-price').text(this.lastElementChild.lastElementChild.lastElementChild.innerHTML);
+    $('.modal-description').text(this.lastElementChild.firstElementChild.innerHTML);
     $('#sup-01').css('background-image', 'url("images/items/' + this.id + '-sup-01.jpg")').val(this.id);
     $('#sup-02').css('background-image', 'url("images/items/' + this.id + '-sup-02.jpg")').val(this.id);
     $('#sup-03').css('background-image', 'url("images/items/' + this.id + '-sup-03.jpg")').val(this.id);
@@ -13,24 +18,9 @@ $(function() {
     $('.gallery-modal').removeClass('modal-activate');
   });
 
-  $('#sup-01').click(function(event) {
-    var value1 = $('#sup-01').val();
-    $('.modal-image').css('background-image', 'url("images/items/' + value1 + '-sup-01.jpg")');
-  });
-
-  $('#sup-02').click(function(event) {
-    var value2 = $('#sup-02').val();
-    $('.modal-image').css('background-image', 'url("images/items/' + value2 + '-sup-02.jpg")');
-  });
-
-  $('#sup-03').click(function(event) {
-    var value3 = $('#sup-03').val();
-    $('.modal-image').css('background-image', 'url("images/items/' + value3 + '-sup-03.jpg")');
-  });
-
-  $('#sup-04').click(function(event) {
-    var value4 = $('#sup-04').val();
-    $('.modal-image').css('background-image', 'url("images/items/' + value4 + '-sup-04.jpg")');
+  $('.suplement').click(function(event) {
+    var value1 = $('.suplement').val();
+    $('.modal-image').css('background-image', 'url("images/items/' + value1 + '-' + this.id + '.jpg")');
   });
 
 });
